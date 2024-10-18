6ade27563a4ab855cc126affa4c602f6c5f81eae
Matteo <106877447+MattSini912@users.noreply.github.com>
2024-10-18 22:10:34 +0200

    Update and rename Template.cpp to template.cpp

---
+++ template.cpp
@@ -0,0 +1,33 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+// #include <fstream>
+#include <string>
+#include <vector>
+#include <limits>
+// #include <algorithm>
+// #include <deque>
+// #include <map>
+// #include <unordered_set>
+// #include <stack>
+// #include <queue>
+using namespace std;
+
+#define el "\n"
+#define ll long long int
+const int MAX_INT = std::numeric_limits<int>::max();
+const int MIN_INT = std::numeric_limits<int>::lowest();
+const int MOD = (1e9)+7;
+
+int main() {
+	// Use cout<<"\n" instead of cout<<endl
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	// ifstream in("input.txt");
+	// ofstream out("output.txt");
+
+	cout << "Hello World!" << el;
+
+	// in.close();
+	// out.close();
+	return 0;
+}
